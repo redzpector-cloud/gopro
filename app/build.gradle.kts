57987f3c501c9662ee979c9a8c461f553b716f16
redzpector-cloud <redzpector@gmail.com>
2026-09-14 09:22:41 +0700

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,19 @@ plugins {
     id("org.jetbrains.kotlin.android")
 }
 
-android { namespace = "com.jejakcam.app"; compileSdk = 35
+android {
+    namespace = "com.jejakcam.app"
+    compileSdk = 35
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
     defaultConfig { applicationId = "com.jejakcam.app"; minSdk = 24; targetSdk = 35; versionCode = 1; versionName = "0.1.0" }
 }
 
